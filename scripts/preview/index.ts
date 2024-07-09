a4af419731674785392da68e4fbb6a5b6295ec83
nintihar <nejc.inti21@gmail.com>
2024-07-09 22:57:26 +0200

    feat: add razor wallet support

--- scripts/preview/index.ts
+++ scripts/preview/index.ts
@@ -3,15 +3,18 @@ import { merge } from "lodash";
 import { EmailClass } from "../utils/email";
 import Twitter from "../utils/twitter/src/twitter";
 import Metamask from "../utils/metamask/src/metamask";
+import Razor from "../utils/razor/src/razor";
 
 interface IOptions {
   entityId: string;
   email?: boolean;
   twitter?: boolean;
   metamask?: boolean;
+  razor?: boolean;
 }
+
 async function main(opts: script.SuperbeesScriptFunctionOptions<IOptions>) {
-  const { entityId, email, twitter, metamask } = opts.vars!;
+  const { entityId, email, twitter, metamask, razor } = opts.vars!;
 
   const entity = await opts.prisma.entity.findUniqueOrThrow({ where: { id: entityId } });
 
@@ -19,6 +22,11 @@ async function main(opts: script.SuperbeesScriptFunctionOptions<IOptions>) {
 
   if (metamask) {
     const extensionPath = await script.utils.resources.getUnpackedExtensionPath("metamask-chrome-11.13.1");
+    args.push(`--disable-extensions-except=${extensionPath}`, `--load-extension=${extensionPath}`);
+  }
+
+  if (razor) {
+    const extensionPath = await script.utils.resources.getUnpackedExtensionPath("Razor-Wallet");
     args.push(`--disable-extensions-except=${extensionPath}`, `--load-extension=${extensionPath}`);
   }
 
@@ -32,6 +40,13 @@ async function main(opts: script.SuperbeesScriptFunctionOptions<IOptions>) {
     const metamask_page = await Metamask.get_metamask_home_page(context);
     const $m: Metamask = await opts.util("metamask", [metamask_page, opts]);
     await $m.import_wallet(wallet_data.mnemonic, wallet_data.password);
+  }
+
+  if (razor) {
+    const wallet_data = await opts.prisma.wallet.findUniqueOrThrow({ where: { type: "MOVE", entityId: opts.entityId } });
+    const razor_page = await Razor.get_razor_home_page(context);
+    const $r: Razor = await opts.util("razor", [razor_page, opts]);
+    await $r.import_wallet(wallet_data.mnemonic, wallet_data.password);
   }
 
   if (email) {
